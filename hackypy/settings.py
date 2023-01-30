be44e37757bc120e6dab96cf7a42586680053377
Recjvenik <himanshum9600@gmail.comm>
2023-01-30 20:25:44 +0530

    allowed host chng

--- hackypy/settings.py
+++ hackypy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zen_^wkajvth-jwx@mx6cgt$1vd)cmn#_udy3$xzsp91w(-n!3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hackypy.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hackypy.herokuapp.com', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
